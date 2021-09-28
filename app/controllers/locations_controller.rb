ab9e76a5b8a4a0735a83c2f8ef621c3b17343905
Gebremeskel <gebre20122@gmail.com>
2021-09-28 16:58:38 -0500

    uncomment serializer

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,8 +2,8 @@ class LocationsController < ApplicationController
     
     def index 
         locations = Location.all
-        render json: locations
-        # render json: LocationSerializer.new(locations, {include: [:trips]})
+        #render json: locations.to_json(except: [:created_at, :updated_at], include: {trips: {only: [:name, :address, :budget]}})
+        render json: LocationSerializer.new(locations, {include: [:trips]})
     end
 
 end

581b4ac71ca517c82a46910ace2a3595deaccf5e
Gebremeskel <gebre20122@gmail.com>
2021-10-06 19:58:37 -0500

    adding create & location Params

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,7 +3,23 @@ class LocationsController < ApplicationController
     def index 
         locations = Location.all
         #render json: locations.to_json(except: [:created_at, :updated_at], include: {trips: {only: [:name, :address, :budget]}})
-        render json: LocationSerializer.new(locations, {include: [:trips]})
+        render json: LocationSerializer.new(locations)
+    end
+
+    def create
+       location = Location.new(location_params)
+        if location.save
+           # render json: location
+            render json: LocationSerializer.new(location)
+        else
+            render json: {error: "Couldn't save"}
+        end
+    end
+
+    private
+
+    def location_params
+        params.require(:location).permit(:name)
     end
 
 end

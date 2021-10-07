8f7cb0a958005969907cc6faea672f99a777feb6
Gebremeskel <gebre20122@gmail.com>
2021-10-06 19:56:52 -0500

    creating location_name attribute methods

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,19 @@
 class Trip < ApplicationRecord
   belongs_to :location
+
+  # def naming
+  #   self.name.upcase
+  # end
+
+  def location_name=(loc_attribute)
+   # byebug
+    if !loc_attribute.blank?
+      self.location = Location.find_or_create_by(name: loc_attribute.upcase) 
+    end
+  end
+
+  def location_name
+    self.location.name
+  end
+
 end

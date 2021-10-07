4ad733018cc4cf10bd170a1313cf895ec974c1bc
Gebremeskel <gebre20122@gmail.com>
2021-10-07 10:21:26 -0500

    change seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# location1 = Location.create(city: "Chicago", state: "IL, USA")
-# location2 = Location.create(city: "Charlotte", state: "NC, USA")
+location1 = Location.create(name: "Down Town Loop")
+location2 = Location.create(name: "Uptown")
 
-# Trip.create(name: "Willis Tower", address: "233 S. Wacker Dr.", budget: 250.55, location: location1)
-# Trip.create(name: "The Art Institute of Chicago", address: "111 S. Michigan Ave.",budget: 550.75, location: location1)
-# Trip.create(name: "Discovery Place Science", address: "301 N Tryon St.", budget: 1250.00, location: location2)
+Trip.create(name: "Willis Tower", address: "233 S. Wacker Dr. Chicago, IL", budget: 250.55, location: location1)
+Trip.create(name: "The Art Institute of Chicago", address: "111 S. Michigan Ave. Chicago, IL",budget: 550.75, location: location1)
+Trip.create(name: "Discovery Place Science", address: "301 N Tryon St. Charlotte, NC", budget: 1250.00, location: location2)
